ecd39ed731d03d082b08456f18cd4bb49d2ddbbe
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-03-01 17:04:54 +0000

    15.1

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-class MyOpen:
-    def __init__(self, filename, mode='r'):
-        self.filename = filename
-        self.mode = mode
+class Employee:
+  raise_amt = 1.04
+  def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
 
-    def __enter__(self):
-        self.fp = open(self.filename, self.mode)
-        return self.fp
+  def fullname(self):
+        return f'{self.first} {self.last}'
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.fp.close()
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amt)
 
+class Developer(Employee):
+  raise_amt = 1.10
 
+  def __init__(self, first, last, pay, lang):
+    super().__init__(first, last, pay)
+    self.lang = lang
 
-with MyOpen('text.txt', 'r') as fp:
-    print(fp.read())
+# dev1 = Developer('Leo', 'Messi', 60_000)
+# foo = 10
+
+# dev1.apply_raise = foo
+# print(dev1.apply_raise)
+# print(dev1.pay)
+
+dev2 = Developer('Leo', 'Messi', 60_000, 'Python')
+dev3 = Developer('Leo', 'Messi', 60_000, 'Java')
